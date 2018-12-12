55e341cfbc7ff3af3d3baba82348e229d7f82eb4
eshamster <hamgoostar@gmail.com>
2018-12-12 12:55:13 +0000

    Update to visualize multiple entities

--- sample/sat/visualizer.lisp
+++ sample/sat/visualizer.lisp
@@ -20,27 +20,36 @@
 (in-package :clw-sample-game-algorithm/sample/SAT/visualizer)
 
 (defstruct.ps+ (visualizer (:include ecs-component))
-  fn-get-projection)
+    fn-get-axis
+  fn-get-target-point-lists)
 
 (defstruct.ps+
     (visualize-system
      (:include ecs-system
-               (target-component-types '(visualizer point-2d params))
+               (target-component-types '(visualizer point-2d))
                (process #'update-visualizer))))
+
+(defvar.ps+ *color-list* (list #xff0000 #x00ffff))
 
 (defun.ps+ update-visualizer (entity)
   (with-ecs-components (visualizer point-2d)
       entity
-    (let ((proj (funcall (visualizer-fn-get-projection visualizer)))
-          (old-proj-model (get-entity-param entity :proj-line-model)))
-      (when old-proj-model
-        (delete-ecs-component old-proj-model entity))
-      (let ((new-proj-model (make-projection-line-model proj)))
-        (add-ecs-component new-proj-model entity)
-        (set-entity-param entity :proj-line-model new-proj-model))
+    (let ((axis (funcall (visualizer-fn-get-axis visualizer)))
+          (target-point-lists
+           (funcall (visualizer-fn-get-target-point-lists visualizer))))
+      (delete-ecs-component-type 'model-2d entity)
+      (let ((count 0))
+        (dolist (point-list target-point-lists)
+          (let* ((proj (project-polygon-to-axis point-list axis))
+                 (new-proj-model (make-projection-line-model
+                                  proj
+                                  (nth (mod count (length *color-list*))
+                                       *color-list*))))
+            (add-ecs-component new-proj-model entity)
+            (incf count))))
       (copy-vector-2d-to
        point-2d
-       (axis-sat-base-point (projection-sat-axis proj))))))
+       (axis-sat-base-point axis)))))
 
 (defun.ps+ init-visualize-system ()
   (register-ecs-system "visualizer" (make-visualize-system))
@@ -53,25 +62,35 @@
      vis
      (make-point-2d)
      (make-visualizer
-      :fn-get-projection (lambda ()
-                           (let* ((entity (find-a-entity-by-tag :sat-object))
-                                  (global-point (calc-global-point entity))
-                                  (global-point-list (list)))
-                             (dolist (point (extract-point-list entity))
-                               (push (transformf-point (clone-point-2d point) global-point)
-                                     global-point-list))
-                             (project-polygon-to-axis
-                              global-point-list
-                              (init-axis-sat (make-point-2d :x 50 :y 50)
-                                             (make-point-2d :x 1 :y 0))))))
+      :fn-get-axis #'get-axis
+      :fn-get-target-point-lists #'get-target-point-lists)
      (init-entity-params))
     (add-ecs-entity vis)))
+
+;; DEBUG
+(defun.ps+ get-axis ()
+  (init-axis-sat (make-point-2d :x 50 :y 50)
+                 (make-point-2d :x 1 :y 0)))
+
+(defun.ps+ get-target-point-lists ()
+  (let ((result (list)))
+    (do-tagged-ecs-entities (entity :sat-object)
+      (push (calc-global-point-list entity) result))
+    (nreverse result)))
+
+(defun.ps+ calc-global-point-list (entity)
+  (let* ((global-point (calc-global-point entity))
+         (global-point-list (list)))
+    (dolist (point (extract-point-list entity))
+      (push (transformf-point (clone-point-2d point) global-point)
+            global-point-list))
+    global-point-list))
 
 ;; DEBUG: copied from sat-system.lisp
 (defun.ps+ extract-point-list (entity)
   (sat-component-point-list (get-ecs-component 'sat-component entity)))
 
-(defun.ps+ make-projection-line-model (proj)
+(defun.ps+ make-projection-line-model (proj color)
   (check-type proj projection-sat)
   (with-slots (axis min max) proj
     (flet ((make-a-point (len)
@@ -82,4 +101,4 @@
        :depth 100
        :model (make-line :pos-a (make-a-point min)
                          :pos-b (make-a-point max)
-                         :color #xff0000)))))
+                         :color color)))))
