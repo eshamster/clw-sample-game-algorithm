dd2d322caf2124ed46cee20fafa3887087cadcb1
eshamster <hamgoostar@gmail.com>
2018-12-30 13:21:22 +0000

    Refactor: Generalize node structure in A* algorighm
    Some refactoring will include change of interface. So they will be
    changed in the next commit.

--- sample/navigation/a-star.lisp
+++ sample/navigation/a-star.lisp
@@ -26,43 +26,32 @@
   goal-node
   grid-state)
 
-(defun.ps+ calc-a-star-index (node a-star)
-  ;; TODO: (Note: this assumes node is grid-mesh-node)
-  (with-slots (x y) node
-    (+ x (* y (grid-mesh-num-x (a-star-mesh a-star))))))
-
 (defun.ps+ get-a-star-grid-state (node a-star)
-  (aref (a-star-grid-state a-star)
-        (calc-a-star-index node a-star)))
+  (gethash (get-node-id (a-star-mesh a-star) node)
+           (a-star-grid-state a-star)))
 
 (defun.ps+ setf-a-star-grid-state (node state a-star)
-  (setf (aref (a-star-grid-state a-star)
-              (calc-a-star-index node a-star))
+  (setf (gethash (get-node-id (a-star-mesh a-star) node)
+                 (a-star-grid-state a-star))
         state))
 
-(defun.ps+ init-a-star (&key nav-mesh start-node goal-node mesh)
-  (let (result)
-    (setf result
-          (make-a-star :goal-node goal-node
-                       :grid-state (make-array (* (nav-mesh-2d-num-x nav-mesh)
-                                                  (nav-mesh-2d-num-y nav-mesh))
-                                               :initial-element nil)
-                       :mesh mesh))
-    (do-nav-mesh-grid ((x y) nav-mesh)
-      (unless (get-nav-mesh-piece-state x y nav-mesh)
-        (setf-a-star-grid-state (make-grid-mesh-node :x x :y y) :closed result)))
+(defun.ps+ init-a-star (&key start-node goal-node mesh)
+  (let ((result
+         (make-a-star :goal-node goal-node
+                      :grid-state (make-hash-table)
+                      :mesh mesh)))
     (open-a-node start-node nil result)
     result))
 
 (defun.ps+ search-path (&key nav-mesh start-x start-y goal-x goal-y (enable-slant-p t))
   "Returns searched path as list like '((x1 y1) ... (xn yn)).
 If path is not found, returns nil."
-  (let ((a-star (init-a-star :nav-mesh nav-mesh
-                             :start-node (make-grid-mesh-node :x start-x :y start-y)
+  (let ((a-star (init-a-star :start-node (make-grid-mesh-node :x start-x :y start-y)
                              :goal-node (make-grid-mesh-node :x goal-x :y goal-y)
                              :mesh (make-grid-mesh :enable-slant-p enable-slant-p
                                                    :num-x (nav-mesh-2d-num-x nav-mesh)
-                                                   :num-y (nav-mesh-2d-num-y nav-mesh)))))
+                                                   :num-y (nav-mesh-2d-num-y nav-mesh)
+                                                   :nav-mesh-2d nav-mesh))))
     (search-path-by-a-star a-star)))
 
 (defun.ps+ search-path-by-a-star (a-star)
@@ -83,6 +72,7 @@ If path is not found, returns nil."
                               (rec parent result)
                               result))))
                (rec goal-node (list)))))
+    ;; TODO
     (let ((goal-node (search-rec)))
       (when goal-node
         (mapcar (lambda (as-node)
@@ -92,55 +82,29 @@ If path is not found, returns nil."
                 (extract-path goal-node))))))
 
 (defun.ps+ goal-node-p (as-node a-star)
-  ;; TODO: This assumes that these nodes are grid-mesh-nodes.
-  ;;       Should be (eq (a-star-node-node as-node) goal-node) after refactoring.
-  (with-slots-pair (((base-x x) (base-y y)) (a-star-node-node as-node)
-                    ((goal-x x) (goal-y y)) (a-star-goal-node a-star))
-    (and (= base-x goal-x)
-         (= base-y goal-y))))
+  (let ((mesh (a-star-mesh a-star)))
+    (eq (get-node-id mesh (a-star-node-node as-node))
+        (get-node-id mesh (a-star-goal-node a-star)))))
 
 (defun.ps+ calc-score (as-node)
   (+ (a-star-node-r-score as-node)
      (a-star-node-h-score as-node)))
-
-(defun.ps+ get-around-node-list (node a-star)
-  (let ((result (list)))
-    (with-slots-pair ((grid-state mesh) a-star
-                      (num-x num-y) mesh
-                      ;; TODO: (Note: this assumes node is grid-mesh-node)
-                      ((base-x x) (base-y y)) node)
-      (labels ((valid-point-p (x y)
-                 (and (<= 0 x (1- num-x))
-                      (<= 0 y (1- num-y))))
-               (push-if-required (x y)
-                 (let ((new-node (make-grid-mesh-node :x x :y y)))
-                   (when (and (valid-point-p x y)
-                              (not (get-a-star-grid-state new-node a-star)))
-                     (push new-node result)))))
-        (push-if-required (1- base-x) base-y)
-        (push-if-required (1+ base-x) base-y)
-        (push-if-required base-x (1- base-y))
-        (push-if-required base-x (1+ base-y))
-        (when (grid-mesh-enable-slant-p mesh)
-          (push-if-required (1- base-x) (1- base-y))
-          (push-if-required (1+ base-x) (1- base-y))
-          (push-if-required (1+ base-x) (1+ base-y))
-          (push-if-required (1- base-x) (1+ base-y)))))
-    result))
 
 (defun.ps+ open-around-nodes (as-node-parent a-star)
   (with-slots (node) as-node-parent
     (setf-a-star-grid-state node :closed a-star)
     (with-slots (opened-nodes) a-star
       (setf opened-nodes (remove as-node-parent opened-nodes)))
-    (dolist (around-node (get-around-node-list node a-star))
-      (open-a-node around-node as-node-parent a-star))))
+    (dolist (around-node (get-around-node-list (a-star-mesh a-star) node))
+      (unless (get-a-star-grid-state around-node a-star)
+        (open-a-node around-node as-node-parent a-star)))))
 
 (defun.ps+ open-a-node (node parent a-star)
   (with-slots (goal-node mesh) a-star
     (let ((as-node (make-a-star-node
                     :node node
                     :parent parent
+                    ;; TODO: Abstract cost between nodes
                     :r-score (if parent (1+ (a-star-node-r-score parent)) 0)
                     :h-score (calc-heuristic-cost
                               mesh node goal-node))))
@@ -162,11 +126,12 @@ If path is not found, returns nil."
 ;; TODO: move to another package
 ;; --- To generalize node --- ;;
 
-(defstruct.ps+ grid-mesh num-x num-y enable-slant-p)
-
-(defstruct.ps+ grid-mesh-node x y)
-
 (defgeneric.ps+ calc-heuristic-cost (mesh node1 node2))
+(defgeneric.ps+ get-around-node-list (mesh node))
+(defgeneric.ps+ get-node-id (mesh node))
+
+(defstruct.ps+ grid-mesh num-x num-y enable-slant-p nav-mesh-2d)
+(defstruct.ps+ grid-mesh-node x y)
 
 (defmethod.ps+ calc-heuristic-cost ((mesh grid-mesh)
                                     (node1 grid-mesh-node) (node2 grid-mesh-node))
@@ -178,3 +143,30 @@ If path is not found, returns nil."
              (abs (- y2 y1)))
         (+ (abs (- x2 x1))
            (abs (- y2 y1))))))
+
+(defmethod.ps+ get-around-node-list ((mesh grid-mesh) (node grid-mesh-node))
+  (let ((result (list)))
+    (with-slots-pair ((num-x num-y nav-mesh-2d) mesh
+                      ((base-x x) (base-y y)) node)
+      (labels ((valid-point-p (x y)
+                 (and (<= 0 x (1- num-x))
+                      (<= 0 y (1- num-y))))
+               (push-if-required (x y)
+                 (let ((new-node (make-grid-mesh-node :x x :y y)))
+                   (when (and (valid-point-p x y)
+                              (get-nav-mesh-piece-state x y nav-mesh-2d))
+                     (push new-node result)))))
+        (push-if-required (1- base-x) base-y)
+        (push-if-required (1+ base-x) base-y)
+        (push-if-required base-x (1- base-y))
+        (push-if-required base-x (1+ base-y))
+        (when (grid-mesh-enable-slant-p mesh)
+          (push-if-required (1- base-x) (1- base-y))
+          (push-if-required (1+ base-x) (1- base-y))
+          (push-if-required (1+ base-x) (1+ base-y))
+          (push-if-required (1- base-x) (1+ base-y)))))
+    result))
+
+(defmethod.ps+ get-node-id ((mesh grid-mesh) (node grid-mesh-node))
+  (with-slots (x y) node
+    (+ x (* y (grid-mesh-num-x mesh)))))
