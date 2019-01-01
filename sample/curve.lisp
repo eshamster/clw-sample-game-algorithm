cc8088334cc386507ed45b4f5af4e779a4eb0f33
eshamster <hamgoostar@gmail.com>
2019-01-01 09:31:31 +0000

    Add empty sample

---
+++ sample/curve.lisp
@@ -0,0 +1,14 @@
+(defpackage :clw-sample-game-algorithm/sample/curve
+  (:use :cl
+        :ps-experiment
+        :cl-ps-ecs
+        :cl-web-2d-game))
+(in-package :clw-sample-game-algorithm/sample/curve)
+
+(clw-sample-game-algorithm/utils:use-this-package-as-sample)
+
+(defun.ps+ init-func (scene)
+  )
+
+(defun.ps+ update-func ()
+  )
